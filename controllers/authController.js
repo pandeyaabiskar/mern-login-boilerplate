47f47433e8fde241cc179f97ba1e58d274c48fca
utkarshashukla-1997 <iutkarshashukla@gmail.com>
2023-01-12 08:20:30 +0545

    Signup,Login,Logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,114 @@
 const User = require('../models/User');
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
+const handleError = (err) =>{
+    const errors= {
+        email:"",
+        password:""
+    } 
+    if(err.message === 'incorrect email'){
+        errors.email="Email is incorrect"
+    }
+    if(err.message==='incorrect password'){
+        errors.password = "Password is incorrect"
+    }
+    //console.log(err)
+ 
+    if(err.code===11000){
+        errors.email="email already exist"
+    }
+    if(err._message==="User validation failed"){
+        errors.email= err.errors.email?err.errors.email.properties.message:""
+        errors.password= err.errors.password?err.errors.password.properties.message:""
+    }
+    return errors
+}
 const returnSignupPage = (req, res) => {
+    res.render('signup')
 }
 
 const returnLoginPage = (req, res) => {
+    res.render('login')
 }
 
 const createUser = async (req, res) => {
+    console.log("the body data",req.body);
+    try{
+    const user = await User.create(req.body);
+     const token = jwt.sign({
+        user:user._id,
+     },process.env.TOKEN_SECRET,{expiresIn:'1d'})
+     res.cookie('jwt', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
+     res.json({
+        message:"user save successfully",
+        user:user,
+
+     })
+    }
+    catch(err){
+        const errors= handleError(err)
+        res.status(500).json({errors})
+
+    }
 
 }
 
-const loginUser = (req, res) => {
-    //Code
+const loginUser = async (req, res) => {
+    //Code 
+    // try{
+    //     const user = await User.create(req.body)
+    //     const token = jwt.sign({
+    //         user:user._id,
+    //      },process.env.TOKEN_SECRET,{expiresIn:'1d'})
+    //      res.cookie('jwt', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
+    //     res.json({
+    //         message:"User login successfully",
+    //         user:user,
+    //     })
+    // } 
+    // catch(err){
+    //     if(err.code !=11000){
+    //         errors.email="Email does not exist"
+    //     }else{
+    //         res.status(500).json({errors})
+    //     }
+       
+        
+
+    // // } 
+    // console.log(req.body)
+    // const {email,password} = req.
+    try{
+        const {email,password} = req.body 
+        const user = await User.findOne({email});
+        if(user){
+            const passwordMatch = await bcrypt.compare(password,user.password);
+            if(passwordMatch){ 
+                const token = jwt.sign({
+                    user:user._id,
+                 },process.env.TOKEN_SECRET,{expiresIn:'1d'})
+                 res.cookie('jwt', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
+                 res.json(
+                  {user:user._id}
+            
+                 )
+
+            } else{
+                throw Error('incorrect password')
+            }
+        } else{
+            throw Error('incorrect email')
+        }             
+    } catch(err){
+        const errors= handleError(err)
+        res.status(500).json({errors})
+    }
+   
 }
 
 const logoutUser = (req, res) => {
-    
+    res.clearCookie('jwt');
+    res.redirect('/login')
 }
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
-
+const validator = require('validator')
+const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
-  //Schema
+  email:{
+   type: String,
+   required: true,
+   unique: true,
+  
+  validate: [validator.isEmail,"Email must be valid"]
+  },
+  password:{
+    type: String,
+    required: [true,"Password is required"],
+    minlength: [6,"Password must be at least 6 characters long"]
+  }
 })
-
+userSchema.pre('save',async function (next){
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt .hash(this.password,salt)
+  next()
+})
 const User = new mongoose.model('User', userSchema);
 
 module.exports = User;
